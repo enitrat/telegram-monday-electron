87fb441d28f6b682aa71372246cf9a8246493ea8
msaug <msaug@protonmail.com>
2023-11-21 21:37:45 +0100

    feat: add icon when sending msg to groups

--- shared/types.ts
+++ shared/types.ts
@@ -1,42 +1,48 @@
 export type MondayBoard = {
-  name: string,
-  id: string,
-  description: string | null,
-  groups: MondayGroup[],
-  columns: MondayColumn[],
+  name: string
+  id: string
+  description: string | null
+  groups: MondayGroup[]
+  columns: MondayColumn[]
   items: any
-
 }
 
 export type MondayGroup = {
-  id: string,
+  id: string
   title: string
 }
 
 export type MondayColumn = {
-  id: string,
+  id: string
   title: string
 }
 
 export type CustomDialog = {
-  date: number;
-  lastMsgDate: number | undefined;
-  link: string;
-  lastMsg: string | undefined;
-  id: bigint;
-  title: string;
-  type: "UserEmpty" | "User" | "Chat" | "Channel" | "ChatEmpty" | "ChatForbidden" | "ChannelForbidden"
+  date: number
+  lastMsgDate: number | undefined
+  link: string
+  lastMsg: string | undefined
+  id: { value: bigInt.BigInteger }
+  title: string
+  type:
+    | 'UserEmpty'
+    | 'User'
+    | 'Chat'
+    | 'Channel'
+    | 'ChatEmpty'
+    | 'ChatForbidden'
+    | 'ChannelForbidden'
 }
 
 export type CustomParticipant = {
-  firstName: string,
-  lastName: string,
-  id: {value:BigInt},
-  username: string,
+  firstName: string
+  lastName: string
+  id: { value: BigInt }
+  username: string
 }
 
 export interface CustomFolder {
-  title: string,
-  peerIds: number[],
-  id: number,
+  title: string
+  peerIds: number[]
+  id: number
 }
